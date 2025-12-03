c28ef232249b1a3e4f6d1c62cf5555f8749023e0
MontiMcFife <101723723+MontiMcFife@users.noreply.github.com>
2025-12-03 10:53:39 +0100

    Update server-post-install.ps1

--- server-post-install.ps1
+++ server-post-install.ps1
@@ -1,1 +1,7 @@
 Write-Host "Server Post Install Done"
+
+Write-Host "Common Post Install Done"
+
+mkdir "c:\test"
+$client = new-object System.Net.WebClient 
+$client.DownloadFile("https://raw.githubusercontent.com/L1-Datenschutz/Digitale-Kanzlei-Bicep-Post-Provisioning/refs/heads/main/server-post-install.ps1","c:\test\server-install.ps1")
