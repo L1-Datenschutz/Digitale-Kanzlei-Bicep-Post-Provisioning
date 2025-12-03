c910b3b94a04ab5acebad6f4950f85c22805c495
MontiMcFife <101723723+MontiMcFife@users.noreply.github.com>
2025-12-03 10:56:18 +0100

    Update sessionhost-post-install.ps1

--- sessionhost-post-install.ps1
+++ sessionhost-post-install.ps1
@@ -1,1 +1,10 @@
 Write-Host "Sessionhost Post Install Done"
+
+try {
+  mkdir "c:\test"
+} catch {
+  cd "c:\test"
+} finally {
+  $client = new-object System.Net.WebClient 
+  $client.DownloadFile("https://raw.githubusercontent.com/L1-Datenschutz/Digitale-Kanzlei-Bicep-Post-Provisioning/refs/heads/main/sessionhost-post-install.ps1","c:\test\sessionhost-install.ps1")
+}
