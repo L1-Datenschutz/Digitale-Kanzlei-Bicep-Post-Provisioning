89bd7cc5e3f02298f1073bad62e959a608dbe545
MontiMcFife <101723723+MontiMcFife@users.noreply.github.com>
2025-12-04 17:38:02 +0100

    Refactor sessionhost-post-install.ps1 script
    Removed directory creation and file download logic.

--- sessionhost-post-install.ps1
+++ sessionhost-post-install.ps1
@@ -4,15 +4,6 @@ param(
 )
 
 try {
-  mkdir "c:\test"
-} catch {
-  cd "c:\test"
-} finally {
-  $client = new-object System.Net.WebClient 
-  $client.DownloadFile("https://raw.githubusercontent.com/L1-Datenschutz/Digitale-Kanzlei-Bicep-Post-Provisioning/refs/heads/main/sessionhost-post-install.ps1","c:\test\sessionhost-install.ps1")
-}
-
-try {
     Write-Host "Installing AVD Agent..."
     Invoke-WebRequest -Uri "https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RWrmXv" -OutFile "AVDAgent.msi"
     Start-Process msiexec.exe -ArgumentList "/i AVDAgent.msi /quiet /norestart REGISTRATIONTOKEN=$RegistrationToken" -Wait
