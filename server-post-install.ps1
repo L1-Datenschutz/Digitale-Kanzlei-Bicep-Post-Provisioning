0609afea76802ebeb8d72312e78dced1c4867461
MontiMcFife <101723723+MontiMcFife@users.noreply.github.com>
2025-12-04 17:38:13 +0100

    Remove unnecessary try-catch for directory creation

--- server-post-install.ps1
+++ server-post-install.ps1
@@ -1,11 +1,1 @@
 Write-Host "Server Post Install Done"
-
-try {
-  mkdir "c:\test"
-} catch {
-  cd "c:\test"
-} finally {
-  $client = new-object System.Net.WebClient 
-  $client.DownloadFile("https://raw.githubusercontent.com/L1-Datenschutz/Digitale-Kanzlei-Bicep-Post-Provisioning/refs/heads/main/server-post-install.ps1","c:\test\server-install.ps1")
-}
-
