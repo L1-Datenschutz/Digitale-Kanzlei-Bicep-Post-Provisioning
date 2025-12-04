4c264a0d44930e339cfb1e00f426291ce9cbe88e
MontiMcFife <101723723+MontiMcFife@users.noreply.github.com>
2025-12-04 17:32:50 +0100

    Remove try-catch block in post-install script

--- common-post-install.ps1
+++ common-post-install.ps1
@@ -1,10 +1,1 @@
 Write-Host "Common Post Install Done"
-
-try {
-  mkdir "c:\test"
-} catch {
-  cd "c:\test"
-} finally {
-  $client = new-object System.Net.WebClient 
-  $client.DownloadFile("https://raw.githubusercontent.com/L1-Datenschutz/Digitale-Kanzlei-Bicep-Post-Provisioning/refs/heads/main/common-post-install.ps1","c:\test\common-install.ps1")
-}
