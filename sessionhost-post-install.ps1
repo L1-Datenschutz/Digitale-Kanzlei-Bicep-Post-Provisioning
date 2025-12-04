7821a57baaf8a6b4b241012f6b27e1b44e55c71f
MontiMcFife <101723723+MontiMcFife@users.noreply.github.com>
2025-12-04 15:34:16 +0100

    Update sessionhost-post-install.ps1

--- sessionhost-post-install.ps1
+++ sessionhost-post-install.ps1
@@ -1,4 +1,7 @@
-Write-Host "Sessionhost Post Install Done"
+param(
+    [Parameter(Mandatory = $true)]
+    [string]$RegistrationToken
+)
 
 try {
   mkdir "c:\test"
@@ -7,4 +10,20 @@ try {
 } finally {
   $client = new-object System.Net.WebClient 
   $client.DownloadFile("https://raw.githubusercontent.com/L1-Datenschutz/Digitale-Kanzlei-Bicep-Post-Provisioning/refs/heads/main/sessionhost-post-install.ps1","c:\test\sessionhost-install.ps1")
+}
+
+try {
+    Write-Host "Installing AVD Agent..."
+    Invoke-WebRequest -Uri "https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RWrmXv" -OutFile "AVDAgent.msi"
+    Start-Process msiexec.exe -ArgumentList "/i AVDAgent.msi /quiet /norestart REGISTRATIONTOKEN=$RegistrationToken" -Wait
+
+    Write-Host "Installing AVD Bootloader..."
+    Invoke-WebRequest -Uri "https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RWrxrH" -OutFile "AVDBootloader.msi"
+    Start-Process msiexec.exe -ArgumentList "/i AVDBootloader.msi /quiet /norestart" -Wait
+
+    Write-Host "VM successfully joined to Host Pool."
+}
+catch {
+    Write-Error "Failed to join Host Pool: $_"
+    exit 1
 }
